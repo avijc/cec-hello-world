423e0ca52275ca8c24f6ef38b0e4728816e44f38
Anssi Johansson <github@miuku.net>
2018-03-02 19:40:21 +0200

    First try for the final application

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,34 @@
 import socket
+import datetime
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
-    return "Hello World! Testing trigger. Greetings from "+socket.gethostname()+"\n"
+    logfile = "/mnt/storedbits/logfile.txt"
+    myname = socket.gethostname()
+    timestamp = str(datetime.datetime.now())
+    writelog(logfile, myname, timestamp)
+    return "Hi visitor! This magnificent Silver Lining webpage was brought to you by " + myname + " at " + \
+        timestamp + ". Here is a log of past visits:<br><br>\n" + getlog(logfile)
 
+def writelog(filename, myname, timestamp):
+    try:
+        log = open(filename, "a")
+        log.write(myname + " " + timestamp + "\n")
+        log.close()
+    except IOError:
+        return "I'm really sorry for the inconvenience, but the log file could not be opened for appending :-/"
+
+def getlog(filename):
+    try:
+        log = open(filename, "r")
+        retval = "<pre>" + log.read() + "</pre>"
+        log.close()
+        return retval
+    except IOError:
+        return "I'm really sorry for the inconvenience, but the log file could not be opened for reading :-/"
 
 if __name__ == "__main__":
     application.run()
